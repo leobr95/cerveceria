a3bd4bda7bcbcc6aa47b00d76ff5955bb0750496
Leonardo Burbano <br.david@outlook.com>
2025-11-06 12:11:31 -0500

    ajuste linter

--- app/sections/HeaderBar.tsx
+++ app/sections/HeaderBar.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { JSX } from 'react';
 import Image from 'next/image';
 import '@/app/styles/HeaderBar.css';
 import logo from '@/app/logos/logo-siete-estrellas.png';
--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { JSX } from 'react';
 import '@/app/styles/Hero.css';
 
 export default function Hero(): JSX.Element {
